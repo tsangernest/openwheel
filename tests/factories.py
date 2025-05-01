ee187b4887d9b3b532f3c780919900629f0991aa
p14s <tsangernest@hotmail.com>
2025-05-01 16:39:14 -0500

    * added driver factory * used method call instead of decorator so it kind of matches what the driver class model looks like

--- tests/factories.py
+++ tests/factories.py
@@ -1,24 +1,25 @@
-from factory import SubFactory, faker, lazy_attribute, post_generation
+from factory import LazyAttribute, faker, post_generation
 from factory.django import DjangoModelFactory
 
 from app.models import Driver, Nationality
 
 
 class DriverFactory(DjangoModelFactory):
-    nationality = faker.Faker("random_element", elements=Nationality.objects.all())
+    ref = LazyAttribute(lambda r: f"{r.surname.lower()}")
     number = faker.Faker("pyint", max_value=99)
+    code = LazyAttribute(lambda c: f"{c.surname[:3].upper()}")
     forename = faker.Faker("first_name")
     surname = faker.Faker("last_name")
     date_of_birth = faker.Faker("date_time_this_century")
-
-    @lazy_attribute
-    def ref(self):
-        return f"{self.surname.lower()}"
-
-    @lazy_attribute
-    def code(self):
-        return f"{self.surname[:3]}"
+    nationality = faker.Faker("random_element", elements=Nationality.objects.all())
+    url = faker.Faker("url")
 
     class Meta:
         model = Driver
+
+    @post_generation
+    def insert_custom_url(self, create, extracted, **kwargs):
+        if not create and not extracted:
+            return
+        self.url = f"{self.url}/{self.surname.lower()}_{self.forename.lower()}"
 
