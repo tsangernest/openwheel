d2c774fcf570655ad6a7ea6c879ee28e16562222
p14s <tsangernest@hotmail.com>
2025-06-13 12:14:34 -0500

    * fixed test for post driver endpoint * test now tests for each field that was entered

--- tests/test_driver.py
+++ tests/test_driver.py
@@ -1,8 +1,10 @@
+from datetime import datetime
+
 import pytest
 from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED
 from rest_framework.test import APIClient
 
-from app.models import Driver
+from app.models import Driver, Nationality
 from tests.factories import DriverFactory
 
 
@@ -37,6 +39,17 @@ def test_post_driver_only_required_fields(drf_c: APIClient):
     }
     response = drf_c.post(path="/driver/", data=driver_payload, format="json")
     assert HTTP_201_CREATED == response.status_code
+    json_response = response.json()
+    assert driver_payload["surname"] == json_response["surname"]
+    assert driver_payload["forename"] == json_response["forename"]
+    assert Nationality.objects.get(id=driver_payload["nationality"]).country == json_response["nationality"]
+    assert (
+        datetime
+        .strptime(driver_payload["date_of_birth"], "%Y-%m-%d")
+        .strftime(format="%d-%b-%Y") == json_response["date_of_birth"]
+    )
+
+    # Test operational
     assert 1 == Driver.objects.count()
     assert 1 == Driver.objects.filter(surname="Targaryen", forename="Daenerys",).count()
 
