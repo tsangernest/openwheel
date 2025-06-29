f91f1089249869ca128304e618578e4ba767efe9
p14s <tsangernest@hotmail.com>
2025-06-29 16:20:04 -0500

    enabled migrations for pytest * wrong number of countries

--- tests/test_migrations/test_normalize_nationality.py
+++ tests/test_migrations/test_normalize_nationality.py
@@ -5,7 +5,7 @@ import pytest
 def test_normalize_nationality(migrator):
     old_state = migrator.apply_initial_migration(("app", "0002_insert_data"))
     old_nationality = old_state.apps.get_model("app", "Nationality")
-    assert old_nationality.objects.count() == 2143
+    assert old_nationality.objects.count() == 2142
 
     new_state = migrator.apply_test_migration(("app", "0003_insert_data"))
     nationality = new_state.apps.get_model("app", "Nationality")
