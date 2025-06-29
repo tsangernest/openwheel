b581d4ecba5eb0cb781c4a32f3684e9c457cc414
p14s <tsangernest@hotmail.com>
2025-06-29 15:07:02 -0500

    enabled migrations for pytest * normalize Nationality

---
+++ tests/test_migrations/test_normalize_nationality.py
@@ -0,0 +1,21 @@
+import pytest
+
+
+@pytest.mark.django_db
+def test_normalize_nationality(migrator):
+    old_nationality_state = migrator.apply_initial_migration(("app", "0002_insert_data"))
+
+    with pytest.raises(expected_exception=LookupError):
+        old_nationality_state.apps.get_model(("app", "Nationality"))
+
+    new_state = migrator.apply_test_migration(("app", "0003_insert_data"))
+    Nationality = new_state.apps.get_model(("app", "Nationality"))
+    assert Nationality.objects.count() == 1502
+
+
+
+
+
+    breakpoint()
+    migrator.reset()
+
