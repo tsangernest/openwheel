487aed090d9aa259f6360dec521997a961c96e58
p14s <tsangernest@hotmail.com>
2025-06-30 09:41:19 -0500

    * normalize db without actually deleting. using deleted_at instead * turned off migration tests

--- tests/test_migrations/test_normalize_nationality.py
+++ tests/test_migrations/test_normalize_nationality.py
@@ -7,11 +7,18 @@ def test_normalize_nationality(migrator):
     old_nationality = old_state.apps.get_model("app", "Nationality")
     assert old_nationality.objects.count() == 2142
 
+    num_of_unique_countries: int = (
+        old_nationality
+        .objects
+        .order_by("country")
+        .distinct("country")
+        .count()
+    )
+
     new_state = migrator.apply_tested_migration(("app", "0003_normalize_nationality"))
     nationality = new_state.apps.get_model("app", "Nationality")
-    assert nationality.objects.count() == 1502
+    assert nationality.objects.count() == num_of_unique_countries + 2142
+    assert nationality.objects.filter(deleted_at__isnull=True).count() == 1502
 
-    breakpoint()
-    print(f"\n\nEnd of processing...")
     migrator.reset()
 
